15975d5f3c2e457f60882949ed7f625b2378b92a
Vaibhav Gupta <vaibhav.gupta12@in.ey.com>
2022-07-17 18:44:42 +0530

    first commit

---
+++ ActuatorExampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorExampleApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ActuatorExampleApplication.class, args);
+	}
+
+}
