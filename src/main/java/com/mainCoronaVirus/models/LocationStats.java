c371430f5c1e60b57f6e23622b9959578764a117
Vaibhav Gupta <vaibhav.gupta12@in.ey.com>
2022-08-20 01:55:11 +0530

    Coronavirus Tracker

---
+++ src/main/java/com/mainCoronaVirus/models/LocationStats.java
@@ -0,0 +1,41 @@
+package com.mainCoronaVirus.models;
+
+public class LocationStats
+{
+    private String state;
+    private String country;
+    private int latestTotalCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+}
